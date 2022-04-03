529c93841f0839a59149246383d513f4f4bf3a9d
Fuchczyk <fuchs.przemyslaw@gmail.com>
2022-04-03 22:01:17 +0200

    Default language - error handling.

--- src/language/language_dictionary.rs
+++ src/language/language_dictionary.rs
@@ -4,6 +4,48 @@ use serde_json;
 
 const LANG_DIR: &str = "./lang/";
 
+const ENG_LANG: &str = r#"{
+    "MAIN_TITLE": "         __   __    __  ___  ____  ___  ___  ____ \n        / _) /  \\  /  \\(   \\(_  _)(  _)/ __)(_  _)\n       ( (/\\( () )( () )) ) ) )(   ) _)\\__ \\  )(  \n        \\__/ \\__/  \\__/(___/ (__) (___)(___/ (__)",
+    "OPTIONS_TITLE": "                    _ (`-.  .-') _                            .-') _   .-')    \n               ( (OO  )(  OO) )                          ( OO ) ) ( OO ).  \n .-'),-----.  _.`     \\/     '._ ,-.-')  .-'),-----. ,--./ ,--,' (_)---\\_) \n( OO'  .-.  '(__...--''|'--...__)|  |OO)( OO'  .-.  '|   \\ |  |\\ /    _ |  \n/   |  | |  | |  /  | |'--.  .--'|  |  \\/   |  | |  ||    \\|  | )\\  :` `.  \n\\_) |  |\\|  | |  |_.' |   |  |   |  |(_/\\_) |  |\\|  ||  .     |/  '..`''.) \n  \\ |  | |  | |  .___.'   |  |  ,|  |_.'  \\ |  | |  ||  |\\    |  .-._)   \\ \n   `'  '-'  ' |  |        |  | (_|  |      `'  '-'  '|  | \\   |  \\       / \n     `-----'  `--'        `--'   `--'        `-----' `--'  `--'   `-----'",
+    "TEST_FOLDER_PATH":       "1)    Test directory:",
+    "PROGRAM_PATH":           "2)      Program path:",
+    "VALGRIND_ACTIVITY":      "3) Valgrind activity:",
+    "STDERR_ACTIVITY":        "4)   Stderr activity:",
+    "LANGUAGE_OPTION":        "5)          Language:",
+    "WARNING_ABSOLUTE_PATH":  "Warning 1: Please use paths of absolute formats!",
+    "WARNING_LANGUAGE":       "Warning 2: Language will change after program restart.",
+    "CHOOSE_OPTION_RETURN6":  "Choose option (6 exits options menu):",
+    "START_TESTS":            "Start testing",
+    "SHOW_SETTINGS":          "Program settings",
+    "EXIT_PROGRAM":           "Exit program",
+    "CHOOSE_OPTION_ENTER":    "Choose an option and press ENTER:",
+    "GET_LANGUAGE":           "Enter a language:",
+    "GET_TEST_PATH":          "Enter a path to folder with tests:",
+    "INCORRECT_PATH":         "Entered path was incorrect!",
+    "GET_PROGRAM_PATH":       "Enter a path to compiled program:",
+    "VALGRIND_USAGE":         "Valgrind usage: (true / false)",
+    "INCORRECT_VALUE":        "Entered value was incorrect!",
+    "STDERR_USAGE":           "Testing of stderr on: (true / false)",
+    "READ_ERROR":             "Reading input error. Try again.",
+    "READ_ERROR_DIGIT":       "Reading input error. Entered digit was incorrect.",
+    "READ_ERROR_NUMBER":      "Reading input error. Entered number was incorrect.",
+    "PROGRAM_END":            "Program is terminating.",
+    "RESULT_ID":              "ID",
+    "RESULT_NAME":            "NAME",
+    "RESULT_TIME":            "TIME",
+    "RESULT_PASSED":          "PASSED",
+    "RESULT_OUTCOME":         "PROBLEM / EXITCODE",
+    "RESULT_TRUE_OUT":        "TRUE",
+    "RESULT_FALSE_OUT":       "FALSE",
+    "RESULT_EXITCODE":        "Program's returned exitcode",
+    "TEST_TOTAL":             "TOTAL",
+    "TEST_PASSED":            "PASSED",
+    "TEST_FAILED":            "FAILED",
+    "TEST_VALGRIND_FAILED":   "VALGRIND FAILED",
+    "TEST_DIFF_FAILED":       "DIFF FAILED",
+    "TEST_OTHER_FAILED":      "OTHER FAIL"
+  }"#;
+
 pub struct LangDict {
     database: HashMap<String, String>
 }
@@ -66,13 +108,7 @@ impl LangDict {
 
 impl Default for LangDict {
     fn default() -> Self {
-        let mut map: HashMap<String, String> = HashMap::new();
-        map.insert("WARNING_ABSOLUTE_PATH".to_string(), "Warning! Please use paths of absolute formats!".to_string());
-        map.insert("TEST_FOLDER_PATH".to_string(), "tak".to_string());
-        map.insert("MAIN_TITLE".to_string(), "          __   __    __  ___  ____  ___  ___  ____ 
-        / _) /  \\  /  \\(   \\(_  _)(  _)/ __)(_  _)
-       ( (/\\( () )( () )) ) ) )(   ) _)\\__ \\  )(  
-        \\__/ \\__/  \\__/(___/ (__) (___)(___/ (__)".to_string());
+        let map = serde_json::from_str(ENG_LANG).unwrap();
         LangDict { database: map }
     }
 }
